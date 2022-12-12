e544a18ad1ac21835d36b77d54ef16e0f5760fc4
ArmineDemirchyan <100238894+ArmineDemirchyan@users.noreply.github.com>
2022-12-12 10:25:32 +0400

    fetching data

--- src/App.js
+++ src/App.js
@@ -9,23 +9,40 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import Login from "./pages/login/Login";
+import { Outlet } from "react-router-dom";
 
+const AppLayout = ({ admin }) =>
+  admin ? (
+    <>
+    <Topbar/>
+      
+      <div className="container">
+        <Sidebar />
+        <Outlet />
+      </div>
+    </>
+  ) : null;
 function App() {
+  const admin = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user || "{}")?.currentUser?.isAdmin ;
   return (
     <BrowserRouter>
-      <Topbar />
-      <div className="container">
-        <Sidebar/>
+      <div className="container2">
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/users" element={<UserList/>}/>
-          <Route path="/user/:userId" element={<User/>}/>
-          <Route path="/newUser" element={<NewUser/>}/>
-          <Route path="/products" element={<ProductList/>}/>
-          <Route path="/product/:productId" element={<Product/>}/>
-          <Route path="/newProduct" element={<NewProduct/>}/>
+          <Route path="/login" element={<Login />} />
+          {admin && (
+              <Route element={<AppLayout admin={admin} />}>
+              <Route index element={<Home />} />
+              <Route path="/users" element={<UserList />} />
+              <Route path="/user/:userId" element={<User />} />
+              <Route path="/newUser" element={<NewUser />} />
+              <Route path="/products" element={<ProductList />} />
+              <Route path="/product/:productId" element={<Product />} />
+              <Route path="/newProduct" element={<NewProduct />} />
+            </Route>
+          )}
+          
         </Routes>
-        
       </div>
     </BrowserRouter>
   );
